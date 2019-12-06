0c75ae0c28250e027403776137be3f54777fc370
Preethy Nair <preethycn89@gmail.com>
2019-12-06 14:26:19 +1100

    fixed code review errors

--- src/main/java/com/companyname/automobile/Application.java
+++ src/main/java/com/companyname/automobile/Application.java
@@ -3,7 +3,6 @@ public class Application {
     public static void main(String[] args) {
         Adder adder = new Adder();
         System.out.println("2 + 2 = " + adder.add(2, 2));
-		addmethod();
     }
 	public void addmethod()
 	{
